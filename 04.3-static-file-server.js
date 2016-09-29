529d46d8b47f3d5c06e33f8ad938e753863418e9
Vargentum <vargentum@gmail.com>
2016-09-29 18:17:00 +0300

    043 add error handling

--- 04.3-static-file-server.js
+++ 04.3-static-file-server.js
@@ -9,6 +9,10 @@ var server = http.createServer(function(req, res) {
   var url = parseUrl(req.url)
   var path = joinPath(root, url.pathname)
   var fileContentStream = fs.createReadStream(path)
+  fileContentStream.on('error', function() {
+    req.statusCode = 500
+    res.end('Internal Server Error')
+  })
   fileContentStream.pipe(res)
 })
 
