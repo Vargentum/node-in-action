1f8f7df89a3f61e27b3569b03eaeea71b69f9ea9
Vargentum <vargentum@gmail.com>
2016-09-29 18:34:11 +0300

    403. add fs.stat call

--- 04.3-static-file-server.js
+++ 04.3-static-file-server.js
@@ -5,15 +5,33 @@ var fs = require('fs')
 
 var root = __dirname
 
+function flush500Error(res) {
+  return function() {
+    res.statusCode = 500
+    res.end('Internal Server Error')
+  }
+}
+
 var server = http.createServer(function(req, res) {
   var url = parseUrl(req.url)
   var path = joinPath(root, url.pathname)
-  var fileContentStream = fs.createReadStream(path)
-  fileContentStream.on('error', function() {
-    req.statusCode = 500
-    res.end('Internal Server Error')
+  var flush500e = flush500Error(res)
+  
+  fs.stat(path, function(err, stat) {
+    if (err) {
+      if (err.name === 'ENOENT') {
+        res.statusCode = 404
+        res.end('File not found')
+      } else {
+        flush500e()
+      }
+    } else {
+      res.setHeader('Content-Length', stat.size)
+      var fileContentStream = fs.createReadStream(path)
+      fileContentStream.on('error', flush500e)
+      fileContentStream.pipe(res)
+    }
   })
-  fileContentStream.pipe(res)
 })
 
 server.listen(9043)
