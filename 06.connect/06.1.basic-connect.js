31599396ea6eda75136307998524bc04c6cc3bf3
Vargentum <vargentum@gmail.com>
2016-10-07 14:57:30 +0300

    add simple middlewares

--- 06.connect/06.1.basic-connect.js
+++ 06.connect/06.1.basic-connect.js
@@ -1,3 +1,23 @@
 var connect = require('connect')
 var app = connect()
+
+app.use(logger)
+   .use(helloWorld)
+
 app.listen(9061)
+
+
+
+/* -----------------------------
+  Middlewares
+----------------------------- */
+
+function logger(req, res, next) {
+  console.log(req.url, req.method)
+  next()
+}
+
+function helloWorld(req, res, next) {
+  res.setHeader('Content-Type', 'text/plain')
+  res.end('Hello world')
+}
