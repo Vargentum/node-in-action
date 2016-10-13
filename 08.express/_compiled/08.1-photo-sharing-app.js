47c3717e6fe4d4cf3d17f47dca47befdd92ae82e
Vargentum <vargentum@gmail.com>
2016-10-13 17:10:02 +0300

    081: scaffold express app

---
+++ 08.express/_compiled/08.1-photo-sharing-app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var _express = require('express');
+
+var _express2 = _interopRequireDefault(_express);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var app = (0, _express2.default)();
+
+app.get('/', function (req, res) {
+  res.send('Hello');
+}).listen(9081);
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uLzA4LjEtcGhvdG8tc2hhcmluZy1hcHAuanMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7QUFBQSxrQ0FBNkI7Ozs7OztBQUU3QixJQUFNLEdBQUcsR0FBRyx3QkFBUyxDQUFBOztBQUdyQixHQUFHLENBQUMsR0FBRyxDQUFDLEdBQUcsRUFBRSxVQUFDLEdBQUcsRUFBRSxHQUFHLEVBQUs7QUFDekIsS0FBRyxDQUFDLElBQUksQ0FBQyxPQUFPLENBQUMsQ0FBQTtDQUNsQixDQUFDLENBQUMsTUFBTSxDQUFDLElBQUksQ0FBQyxDQUFBIiwiZmlsZSI6IjA4LjEtcGhvdG8tc2hhcmluZy1hcHAuanMiLCJzb3VyY2VzQ29udGVudCI6WyJpbXBvcnQgZXhwcmVzcyBmcm9tICdleHByZXNzJ1xuXG5jb25zdCBhcHAgPSBleHByZXNzKClcblxuXG5hcHAuZ2V0KCcvJywgKHJlcSwgcmVzKSA9PiB7XG4gIHJlcy5zZW5kKCdIZWxsbycpXG59KS5saXN0ZW4oOTA4MSkiXX0=
